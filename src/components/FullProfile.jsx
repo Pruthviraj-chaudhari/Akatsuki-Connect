07bc71441e255742e58562d83efff5492d784869
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2024-03-01 23:37:38 +0530

    Edite sharing message

--- src/components/FullProfile.jsx
+++ src/components/FullProfile.jsx
@@ -73,7 +73,7 @@ const FullProfile = () => {
       if (navigator.share) {
         await navigator.share({
           title: 'Share Akatsuki Connect Profile',
-          text: "Join Akatsuki Coding Club on Akatsuki Connect! 🚀 Let's code together and showcase our skills.",
+          text: "Discover my coding journey on Akatsuki Connect! 🚀\nExplore my profile and join the Akatsuki Coding Club community.\nReady to showcase your skills? Create your own profile now!\n\n",
           url: currentUrl, 
         });
       } else {
