8b4cb690a8da44622dca3fb4e5db0664fa507685
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2024-03-12 00:49:31 +0530

    Removed humberger hover effect

--- src/components/Humberger.jsx
+++ src/components/Humberger.jsx
@@ -3,7 +3,7 @@
 const Humberger = ({setOpen}) => {
   return (
     <button
-          className="text-white inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 py-2 mr-2 px-0 text-base hover:bg-white focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+          className="text-white inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-9 py-2 mr-2 px-0 text-base focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
           type="button"
           aria-haspopup="dialog"
           aria-expanded="false"
